687ab2bf7a7cf78c87bce8c66ae93be02ca1be42
Alexandre Narbonne <alexandre.narbonne@gadz.org>
2016-10-05 15:23:51 +0200

    Add SSL scheme

--- config/initializers/activeldap.rb
+++ config/initializers/activeldap.rb
@@ -4,5 +4,6 @@ ActiveLdap::Base.setup_connection host: GorgLdapDaemon.config["ldap_host"],
                                   bind_dn: GorgLdapDaemon.config["ldap_bind_dn"],
                                   password: GorgLdapDaemon.config["ldap_password"],
                                   logger: GorgLdapDaemon.logger,
+                                  method: (GorgLdapDaemon.config["ldap_connection_method"] && GorgLdapDaemon.config["ldap_connection_method"].to_sym)||:plain,
                                   retry_limit: 0,
                                   retry_wait: 0
